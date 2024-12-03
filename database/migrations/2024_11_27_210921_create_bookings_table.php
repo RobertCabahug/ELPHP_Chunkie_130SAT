20055337f33d4ae5b7f182813fd6edbe1e0c14a0
jember7 <jembermanacap@gmail.com>
2024-12-03 14:19:09 +0800

    commit

--- database/migrations/2024_11_27_210921_create_bookings_table.php
+++ database/migrations/2024_11_27_210921_create_bookings_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
         $table->string('expert_address');
         $table->string('expert_image_url')->nullable();
         $table->string('user_address');
-        $table->timestamps();
+    
 
         // Foreign keys to link to users
         $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
